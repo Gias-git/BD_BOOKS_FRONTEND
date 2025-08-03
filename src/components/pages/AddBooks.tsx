ddf5b06f5c6883614c78f5ba42e05e45647bd365
Gias-git <giasuddin.programmer@gmail.com>
2025-08-03 20:43:40 +0600

    form changes

--- src/components/pages/AddBooks.tsx
+++ src/components/pages/AddBooks.tsx
@@ -5,7 +5,7 @@ const AddBooks = () => {
     return (
         <div>
             <h1 className="my-14 text-center text-2xl font-bold ">Add Your Book</h1>
-            <div className="w-[500px] rounded-3xl mx-auto border-2 border-gray-200 shadow-md p-10">
+            <div className="lg:w-[500px] w-10/12 rounded-3xl mx-auto border-2 border-gray-200 shadow-md p-10">
                 <AddBooksForm></AddBooksForm>
             </div>
         </div>
